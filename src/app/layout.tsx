bd6ed087cad88f1fd6bd92f242f306de24ee07fa
Somnath Mishra <somnathmishra100dbi@gmail.com>
2026-06-29 22:55:15 +0530

    updated outdated versions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
           <ConvexClientProvider>
             <ThemeProvider
               attribute="class"
-              defaultTheme="system"
+              defaultTheme="dark"
               enableSystem
               disableTransitionOnChange
             >
